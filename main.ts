b1c6ee4106a830a3c6db7e2d18025d4f811e5454
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2024-10-03 18:37:10 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,33 @@
 input.onButtonPressed(Button.A, function () {
     start = 1
 })
-function test (num: number) {
-    range = strip.range(num, 1)
-    range.setBrightness(255 / (8 - num))
-    range.showColor(neopixel.hsl(275, 100, 10 + num * 6))
-    range.show()
-}
 input.onButtonPressed(Button.B, function () {
     start = 0
 })
-let range: neopixel.Strip = null
 let start = 0
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 32, NeoPixelMode.RGB)
-for (let index = 0; index <= 7; index++) {
-    test(index)
-}
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . # # # .
+    # # # # #
+    . # . # .
+    `)
+let delka_komety = 1
+let delka_pasku = 5
+let pasek1 = neopixel.create(DigitalPin.P0, 4 * delka_pasku + 15, NeoPixelMode.RGB)
+let rada1 = pasek1.range(10, delka_pasku * 2)
+let kometa1 = rada1.range(0, delka_komety)
+kometa1.showColor(neopixel.colors(NeoPixelColors.Indigo))
+let rada2 = pasek1.range(25, 10)
+let kometa2 = rada2.range(9, delka_komety)
+kometa2.showColor(neopixel.colors(NeoPixelColors.Indigo))
 start = 0
 basic.forever(function () {
     if (start == 1) {
-        strip.rotate(1)
-        strip.show()
-        basic.pause(20)
+        rada1.rotate(1)
+        rada2.rotate(-1)
+        rada1.show()
+        rada2.show()
+        basic.pause(150)
     }
 })
